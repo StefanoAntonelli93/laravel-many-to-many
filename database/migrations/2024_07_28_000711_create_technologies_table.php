5c8519374b7bbf3b334785cdfa7deb498cf1d5c9
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-07-28 02:22:50 +0200

    feat: add Technology migration and seeder

--- database/migrations/2024_07_28_000711_create_technologies_table.php
+++ database/migrations/2024_07_28_000711_create_technologies_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('technologies', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 50)->unique();
+            $table->string('slug', 120)->nullable();
             $table->timestamps();
         });
     }
--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -12,6 +15,25 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // disabilito relazione tra tabelle se voglio usare truncate perchè non posso eliminare dei campi che hanno relazioni
+        Schema::disableForeignKeyConstraints();
+        // svuoto tabella per non duplicare seed
+        Technology::truncate();
+
+        // popolo technology con array
+        $technologies = ['Frontend', 'Backend', 'Big Data', 'Cloud', 'IA', 'Machine learning'];
+
+        // ciclo per popolare entità technology
+        foreach ($technologies as $technology) {
+            $new_technology = new Technology();
+
+            $new_technology->name = $technology;
+            $new_technology->slug = Str::of($new_technology->name)->slug();
+
+            // salvo e lancio istanze nel database
+            $new_technology->save();
+        }
+        //  dopo operazioni riabilito relazione tra tabelle type e project
+        Schema::enableForeignKeyConstraints();
     }
 }
